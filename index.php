b0d1390eed0fa351c442f76fc7ee525c0b3fe024
Roy Vanegas <roy@thecodeeducators.com>
2024-10-28 11:10:12 -0400

    Add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Apple Macintosh Computer Inventory</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,200;0,500;1,200;1,500&display=swap">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+</body>
+</html>
